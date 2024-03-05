602fda8d8bc9b344bfcf575e1717d1ef6aad3cab
BuiDuongKham <129467586+BuiDuongKham@users.noreply.github.com>
2024-03-05 14:41:05 +0700

    fix bug when deleting local storage

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,84 @@
 import { User } from '../components/UserList';
 import { Product } from '../components/ProductList';
-const host = import.meta.env.VITE_HOST || 'https://nest-mongo-gold.vercel.app';
+const host = import.meta.env.VITE_HOST;
 const token = localStorage.getItem('accessToken');
-if (!token) {
-  window.location.href = '/login';
-}
+const refreshToken = localStorage.getItem('refreshToken');
+
+const isTokenExist = () => {
+  return token || refreshToken;
+};
+
+const redirectToLoginIfTokenNotExist = () => {
+  if (!isTokenExist) {
+    window.location.href = '/login';
+  }
+};
+
+export const IsLogin = async () => {
+  if (!isTokenExist) return false;
+  const res = await getUsers();
+  if (res.status === 401) return false;
+  return true;
+};
+
+export const login = async (username: string, password: string) => {
+  try {
+    const res = await fetch(`${host}/auth/signin`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        // CORS
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH, HEAD, OPTIONS',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    console.log(res);
+    const data = await res.json();
+    console.log(data);
+    if (data.access_token && data.refresh_token) {
+      const accessToken = data.access_token;
+      const refreshToken = data.refresh_token;
+      localStorage.setItem('accessToken', accessToken);
+      localStorage.setItem('refreshToken', refreshToken);
+      window.location.href = '/';
+    } else {
+      alert(data.message  );
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const refresh = async () => {
+  try {
+    const res = await fetch(`${host}/auth/refresh-token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ refreshToken }),
+    });
+
+    if (res.status !== 401) {
+      const data = await res.json();
+      if (data.access_token && data.refresh_token) {
+        const accessToken = data.access_token;
+        const refreshToken = data.refresh_token;
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
+        window.location.href = '/';
+      } else {
+        console.log('error');
+      }
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 export const getUsers = async () => {
+  redirectToLoginIfTokenNotExist();
   return await fetch(`${host}/users`, {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -16,6 +88,7 @@ export const getUsers = async () => {
 };
 
 export const createUser = async (user: User) => {
+  redirectToLoginIfTokenNotExist();
   return await fetch(`${host}/users`, {
     body: JSON.stringify({
       name: user.name || '',
@@ -34,6 +107,7 @@ export const createUser = async (user: User) => {
 };
 
 export const modifyUser = async (user: User) => {
+  redirectToLoginIfTokenNotExist();
   return await fetch(`${host}/users/${user.id}`, {
     body: JSON.stringify({
       name: user.name || '',
@@ -52,6 +126,7 @@ export const modifyUser = async (user: User) => {
 };
 
 export const deleteUser = async (user: User) => {
+  redirectToLoginIfTokenNotExist();
   return await fetch(`${host}/users/${user.id}`, {
     method: 'DELETE',
     headers: {
@@ -62,6 +137,7 @@ export const deleteUser = async (user: User) => {
 };
 
 export const getProducts = async () => {
+  redirectToLoginIfTokenNotExist();
   return await fetch(`${host}/products`, {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -71,6 +147,7 @@ export const getProducts = async () => {
 };
 
 export const createProduct = async (product: Product) => {
+  redirectToLoginIfTokenNotExist();
   return await fetch(`${host}/products`, {
     body: JSON.stringify({
       name: product.name || '',
@@ -89,6 +166,7 @@ export const createProduct = async (product: Product) => {
 };
 
 export const modifyProduct = async (product: Product) => {
+  redirectToLoginIfTokenNotExist();
   return await fetch(`${host}/products/${product.id}`, {
     body: JSON.stringify({
       name: product.name || '',
@@ -107,6 +185,7 @@ export const modifyProduct = async (product: Product) => {
 };
 
 export const deleteProduct = async (product: Product) => {
+  redirectToLoginIfTokenNotExist();
   return await fetch(`${host}/products/${product.id}`, {
     method: 'DELETE',
     headers: {
